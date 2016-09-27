3f36a73bdc09e2f106e9c0dfac82b96922ea2bdb
Shyam <shyam@shyam.id.au>
2016-09-27 15:03:50 +1000

    Trying to use 3JS to read JSON into a global.

--- euclidian/euclidian.js
+++ euclidian/euclidian.js
@@ -36,23 +36,22 @@ var euclidian = SAGE2_WebGLApp.extend({
         this.scene.fog=new THREE.FogExp2(fogHex, fogDensity);
         this.geometry=new THREE.Geometry();
 
-        var mydata = (function() {
-            var file = null;
-            console.log(file);
-            $.ajax({
-                'async': false,
-                'global': false,
-                'url': "http://43.240.97.247:80/data.json",
-                'dataType': "json",
-                'success': function (data) {console.log(data);}
-            });
-            return file;
-        })();
+        var loader = new THREE.JSONLoader();
+        loader.load('data.json', function(geometry) {
+            /*
+            var mesh = new THREE.Mesh(geometry, new THREE.MeshBasicMaterial());
+            mesh.position.x =500;
+            mesh.position.y =100;
+            mesh.position.z =500;
+            scene.add(mesh);
+            */
+            this.mydata = geometry;
+        });
 
-        var particleCount=mydata.length;
+        var particleCount=this.mydata.length;
 
         for (var i=0;i<particleCount;i++) {
-            var coOrd=mydata[i];
+            var coOrd=this.mydata[i];
             var vertex = new THREE.Vector3();
             vertex.x = coOrd[0];
             vertex.y = coOrd[1];
