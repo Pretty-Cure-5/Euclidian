f89a49bfc81564ce8abec8e90b75477db67307d0
Shyam <shyam@shyam.id.au>
2016-09-29 11:47:07 +1000

    IP addresses interface should be operational.

--- Server/ip_address.sh
+++ Server/ip_address.sh
@@ -2,5 +2,8 @@
 # This script rules in favour of an IP address.
 #   ./ip_address.sh $ip_address
 
+# https://en.wikipedia.org/wiki/List_of_TCP_and_UDP_port_numbers
+ports=22,80,443,1337,9090,9292
+
 sudo iptables -A INPUT  -p tcp -s $1/32 --match multiport --dports $ports -j ACCEPT
 sudo iptables -A OUTPUT -p tcp -d $1/32 -j ACCEPT
--- Server/sys.js
+++ Server/sys.js
@@ -6,56 +6,96 @@ console.log("Maintenance script...");
 
 ////////////////////////////////////////////////////////////////////////////////
 console.log("Importing modules...");
-const http = require("http");
-const url = require('url');
-const exec = require('child_process').exec;
+const
+    http = require("http")
+    , url = require('url')
+    , exec = require('child_process').exec
+    , queryString = require('querystring')
+;
 
 ////////////////////////////////////////////////////////////////////////////////
 console.log("Creating server...");
 http.createServer(function(request, response) {
     function r(body) {
-        var head = '<!DOCTYPE html><html><head><title>Pretty Cure Server</title><meta charset="utf-8"></head><body>';
+        var head = [
+            '<!DOCTYPE html>'
+            , '<html><head>'
+            , '<meta charset="utf-8">'
+            , '<title>Pretty Cure Server</title>'
+            , '<link rel="shortcut icon" type="image/x-icon" href="favicon.ico"/>'
+            , '<style>'
+            , '*{font-family:monospace,sans-serif;}'
+            , '</style>'
+            , '</head><body>'
+        ];
         var tail = '</body></html>';
-
-        // Send the HTTP header
-        // HTTP Status: 200 : OK
-        // Content Type: text/plain
-        response.writeHead(200, {'Content-Type': 'text/html'});// 'text/plain'
-
-        // Send the response body as "Hello World"
-        response.write(head + body.toString() + tail);
-        //response.end();
+        // HTTP header
+        response.writeHead(200, {'Content-Type': 'text/html'});
+        // HTML source
+        response.write(head.join('') + body.toString() + tail);
     }
 
     // Parse the request containing file name
-    var pathname = url.parse(request.url).pathname;
+    var parsely    = url.parse(request.url)
+        , pathname = parsely.pathname.substr(1)
+        , query    = queryString.parse(parsely.query)
+    ;
 
     // Print the name of the file for which request is made.
-    console.log("Request for " + pathname.substr(1) + " received.");
+    console.log("Request for " + pathname + " received.");
 
-    switch(pathname.substr(1)) {
+    var address = 'asdf';
+    switch(pathname) {
         case 'sync':
-            const child = exec('sh /home/ubuntu/Euclidian/Server/sync.sh', (error, stdout, stderr) => {
-                //if (error) {console.log(error);}
-                //console.log(stdout);
-                if (error) {
-                    console.error(`exec error: ${error}`);
-                    return;
+            const child = exec(
+                'sh /home/ubuntu/Euclidian/Server/sync.sh'
+                , (error, stdout, stderr) => {
+                    if (error) {
+                        console.error(`exec error: ${error}`);
+                        return;
+                    }
+                    console.log('Synchronised! :D');
                 }
-                //console.log(`stdout: ${stdout}`);
-                //console.log(`stderr: ${stderr}`);
-                console.log('Synchronised! :D');
-            });
-            r('SAGE2 Apps have been synchronised! :D');
+            );
+            r('<span style="color:green">SAGE2 Apps have been synchronised! :D</span>');
+            break;
+        case 'firewall':
+            var keys = Object.keys(query);
+            if(keys.length == 1) {
+                if(keys[0] == 'ip') {
+                    if(/^\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}$/.test(query.ip)) {
+                        r('<span style="color:green">' + query.ip + ' has been invited to the Pretty Cure party! :D</span>');
+                        const child = exec(
+                            'sh /home/ubuntu/Euclidian/Server/ip_address.sh ' + query.ip
+                            , (error, stdout, stderr) => {
+                                if (error) {
+                                    console.error(`exec error: ${error}`);
+                                    return;
+                                }
+                                console.log(query.ip + '; Invited! :D');
+                            }
+                        );
+                    } else {
+                        r('<span style="color:red">ERR: Invalid IP address!</span>');
+                    }
+                } else {
+                    r('<span style="color:red">ERR: Unrecognised variable!</span>');
+                }
+            } else {
+                if(keys.length < 1) {
+                    r('<span style="color:red">ERR: Missing parameter!</span>');
+                } else {
+                    r('<span style="color:red">ERR: Too many parameters!</span>');
+                }
+            }
             break;
         default:
-            r('Hello World! :D\n');
+            r('Hello World! :D');
             break;
     }
     response.end();
 }).listen(1337);/** Port 1337 **/// https://en.wikipedia.org/wiki/List_of_TCP_and_UDP_port_numbers
 
-// Console will print the message
 console.log('Server running at http://43.240.97.247:1337/');
 
 ////////////////////////////////////////////////////////////////////////////////
