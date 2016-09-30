2d27f39a92fda18636c582622e44d5973be9e521
Shyam <shyam@shyam.id.au>
2016-10-01 03:58:38 +1000

    The 1337 Server is 1337.

---
+++ Server/mod.js
@@ -0,0 +1,102 @@
+const fs = require('fs');
+
+exports.checkExternal = (request) => {
+    return request.url.startsWith('/bro/');
+};
+
+exports.handleExternal = (request, response) => {
+    let url = request.url.substr(1);
+    // readFileSync is not asynchronous; this may come in handy...
+    fs.readFile(url, (error, data) => {
+        if(error) {
+            // https://en.wikipedia.org/wiki/List_of_HTTP_status_codes
+            console.log(`HTTP 404: Not Found!\n${error}`);
+            response.writeHead(404, {'Content-Type': 'text/plain'});
+            response.end(`HTTP 404: Not Found!\n${error}`);
+        } else {
+            response.statusCode = 200;
+            response.end(data);
+        }
+    });
+};
+
+exports.handleInternal = (request, response) => {
+    function r(body) {
+        let head = [
+            '<!DOCTYPE html>'
+            , '<html><head>'
+            , '<meta charset="utf-8"/>'
+            , '<title>Pretty Cure Server</title>'
+            , '<link rel="shortcut icon" type="image/x-icon" href="/bro/favicon.ico"/>'
+            , '<style>'
+            , '*{font-family:monospace,sans-serif;}'
+            , '</style>'
+            , '</head><body>'
+        ];
+        var tail = '</body></html>';
+        // HTTP header
+        response.writeHead(200, {'Content-Type': 'text/html'});
+        // HTML source
+        response.write(head.join('')  );
+        response.write(body.toString());
+        response.end  (tail           );
+    }
+
+    let parsely    = url.parse(request.url)
+        , pathname = parsely.pathname.substr(1)
+        , query    = queryString.parse(parsely.query)
+    ;
+
+    switch(pathname) {
+        case 'sync':
+            let child = exec(
+                'sh /home/ubuntu/Euclidian/Server/sync.sh'
+                , (error, stdout, stderr) => {
+                    if (error) {
+                        console.error(`exec error: ${error}`);
+                        return;
+                    }
+                    console.log('Synchronised! :D');
+                }
+            );
+            r('<span style="color:green">SAGE2 Apps have been synchronised! :D</span>');
+            break;
+        case 'firewall':
+            let keys = Object.keys(query);
+            if(keys.length == 1) {
+                if(keys[0] == 'ip') {
+                    if(/^\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}$/.test(query.ip)) {
+                        r(
+                            '<span style="color:green">'
+                            + query.ip
+                            + ' has been invited to the Pretty Cure party! :D</span>'
+                        );
+                        let child = exec(
+                            'sh /home/ubuntu/Euclidian/Server/ip_address.sh ' + query.ip
+                            , (error, stdout, stderr) => {
+                                if (error) {
+                                    console.error(`exec error: ${error}`);
+                                    return;
+                                }
+                                console.log(query.ip + '; Invited! :D');
+                            }
+                        );
+                    } else {
+                        r('<span style="color:red">ERR: Invalid IP address!</span>');
+                    }
+                } else {
+                    r('<span style="color:red">ERR: Unrecognised variable!</span>');
+                }
+            } else {
+                if(keys.length < 1) {
+                    r('<span style="color:red">ERR: Missing parameter!</span>');
+                } else {
+                    r('<span style="color:red">ERR: Too many parameters!</span>');
+                }
+            }
+            break;
+        default:
+            r('Hello World! :D');
+            break;
+    }
+};
--- Server/sys.js
+++ Server/sys.js
@@ -2,100 +2,39 @@
 nohup node sys.js -li &
 vim sys.js; node sys.js -li
 */
-console.log("Maintenance script...");
+console.log('Booting the 1337 server...');
 
 ////////////////////////////////////////////////////////////////////////////////
-console.log("Importing modules...");
+console.log('Importing modules...');
 const
-    http = require("http")
+    http = require('http')
     , url = require('url')
     , exec = require('child_process').exec
     , queryString = require('querystring')
+    , mod = require('./mod.js')
+;
+
+// $ ifconfig
+// https://en.wikipedia.org/wiki/List_of_TCP_and_UDP_port_numbers
+console.log('Declaring constants...');
+const
+    server = '43.240.97.247'
+    , port = 1337
 ;
 
 ////////////////////////////////////////////////////////////////////////////////
-console.log("Creating server...");
+console.log('Creating server...');
 http.createServer(function(request, response) {
-    function r(body) {
-        var head = [
-            '<!DOCTYPE html>'
-            , '<html><head>'
-            , '<meta charset="utf-8">'
-            , '<title>Pretty Cure Server</title>'
-            , '<link rel="shortcut icon" type="image/x-icon" href="favicon.ico"/>'
-            , '<style>'
-            , '*{font-family:monospace,sans-serif;}'
-            , '</style>'
-            , '</head><body>'
-        ];
-        var tail = '</body></html>';
-        // HTTP header
-        response.writeHead(200, {'Content-Type': 'text/html'});
-        // HTML source
-        response.write(head.join('') + body.toString() + tail);
+    console.log(`Incoming request: ${request.url}`);
+    if(mod.checkExternal(request)) {
+        // external files (from the /bro/ directory)
+        mod.handleExternal(request, response);
+    } else {
+        // internal files (address bar = commandline interface)
+        mod.handleInternal(request, response);
     }
-
-    // Parse the request containing file name
-    var parsely    = url.parse(request.url)
-        , pathname = parsely.pathname.substr(1)
-        , query    = queryString.parse(parsely.query)
-    ;
-
-    // Print the name of the file for which request is made.
-    console.log("Request for " + pathname + " received.");
-
-    var address = 'asdf';
-    switch(pathname) {
-        case 'sync':
-            const child = exec(
-                'sh /home/ubuntu/Euclidian/Server/sync.sh'
-                , (error, stdout, stderr) => {
-                    if (error) {
-                        console.error(`exec error: ${error}`);
-                        return;
-                    }
-                    console.log('Synchronised! :D');
-                }
-            );
-            r('<span style="color:green">SAGE2 Apps have been synchronised! :D</span>');
-            break;
-        case 'firewall':
-            var keys = Object.keys(query);
-            if(keys.length == 1) {
-                if(keys[0] == 'ip') {
-                    if(/^\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}$/.test(query.ip)) {
-                        r('<span style="color:green">' + query.ip + ' has been invited to the Pretty Cure party! :D</span>');
-                        const child = exec(
-                            'sh /home/ubuntu/Euclidian/Server/ip_address.sh ' + query.ip
-                            , (error, stdout, stderr) => {
-                                if (error) {
-                                    console.error(`exec error: ${error}`);
-                                    return;
-                                }
-                                console.log(query.ip + '; Invited! :D');
-                            }
-                        );
-                    } else {
-                        r('<span style="color:red">ERR: Invalid IP address!</span>');
-                    }
-                } else {
-                    r('<span style="color:red">ERR: Unrecognised variable!</span>');
-                }
-            } else {
-                if(keys.length < 1) {
-                    r('<span style="color:red">ERR: Missing parameter!</span>');
-                } else {
-                    r('<span style="color:red">ERR: Too many parameters!</span>');
-                }
-            }
-            break;
-        default:
-            r('Hello World! :D');
-            break;
-    }
-    response.end();
-}).listen(1337);/** Port 1337 **/// https://en.wikipedia.org/wiki/List_of_TCP_and_UDP_port_numbers
-
-console.log('Server running at http://43.240.97.247:1337/');
+}).listen(port, server, () => {
+    console.log(`Server listening to http://${server}:${port}/`);
+});
 
 ////////////////////////////////////////////////////////////////////////////////
