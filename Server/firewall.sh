35ebd53e2536128e77ec65795fe916a69b41d87f
Sharminator <s3421215@student.rmit.edu.au>
2016-10-07 17:02:28 +1100

    PCS: Installation is more streamlined.

--- Server/firewall.sh
+++ Server/firewall.sh
@@ -1,71 +1,72 @@
 #!/usr/bin/env bash
 # Using iptables instead of NeCTAR's unconventional interface.
 
-phonebook=/home/ubuntu/phonebook
+home=/home/ubuntu
+phonebook="$home"/phonebook
 # https://en.wikipedia.org/wiki/List_of_TCP_and_UDP_port_numbers
 ports=22,80,443,1337,9090,9292
 
 # Defenestration.
 ip6tables -F;
-ip6tables -X;
-iptables  -F;
-iptables  -X;
+sudo ip6tables -X;
+sudo iptables  -F;
+sudo iptables  -X;
 
 # Conflagration.
-ip6tables -P INPUT   DROP;
-ip6tables -P OUTPUT  DROP;
-ip6tables -P FORWARD DROP;
-iptables  -P INPUT   DROP;
-iptables  -P OUTPUT  DROP;
-iptables  -P FORWARD DROP;
+sudo ip6tables -P INPUT   DROP;
+sudo ip6tables -P OUTPUT  DROP;
+sudo ip6tables -P FORWARD DROP;
+sudo iptables  -P INPUT   DROP;
+sudo iptables  -P OUTPUT  DROP;
+sudo iptables  -P FORWARD DROP;
 
 # Stingers.
-ip6tables -A INPUT   -m state --state INVALID -j REJECT;
-ip6tables -A FORWARD -m state --state INVALID -j REJECT;
-ip6tables -A OUTPUT  -m state --state INVALID -j REJECT;
-iptables  -A INPUT   -m state --state INVALID -j REJECT;
-iptables  -A FORWARD -m state --state INVALID -j REJECT;
-iptables  -A OUTPUT  -m state --state INVALID -j REJECT;
-iptables  -A INPUT   -f -j REJECT;
-iptables  -A FORWARD -f -j REJECT;
-iptables  -A OUTPUT  -f -j REJECT;
+sudo ip6tables -A INPUT   -m state --state INVALID -j REJECT;
+sudo ip6tables -A FORWARD -m state --state INVALID -j REJECT;
+sudo ip6tables -A OUTPUT  -m state --state INVALID -j REJECT;
+sudo iptables  -A INPUT   -m state --state INVALID -j REJECT;
+sudo iptables  -A FORWARD -m state --state INVALID -j REJECT;
+sudo iptables  -A OUTPUT  -m state --state INVALID -j REJECT;
+sudo iptables  -A INPUT   -f -j REJECT;
+sudo iptables  -A FORWARD -f -j REJECT;
+sudo iptables  -A OUTPUT  -f -j REJECT;
 
 # Ping. *quack-bzz-quack-quack-quack-quack-quack-quack-quack-quack*
 # http://www.cyberciti.biz/tips/linux-iptables-9-allow-icmp-ping.html
-ip6tables -A INPUT  -p icmpv6 --icmpv6-type echo-request -j ACCEPT
-ip6tables -A OUTPUT -p icmpv6 --icmpv6-type echo-reply   -j ACCEPT
-ip6tables -A OUTPUT -p icmpv6 --icmpv6-type echo-request -j ACCEPT
-ip6tables -A INPUT  -p icmpv6 --icmpv6-type echo-reply   -j ACCEPT
-iptables  -A INPUT  -p icmp   --icmp-type   echo-request -j ACCEPT
-iptables  -A OUTPUT -p icmp   --icmp-type   echo-reply   -j ACCEPT
-iptables  -A OUTPUT -p icmp   --icmp-type   echo-request -j ACCEPT
-iptables  -A INPUT  -p icmp   --icmp-type   echo-reply   -j ACCEPT
+sudo ip6tables -A INPUT  -p icmpv6 --icmpv6-type echo-request -j ACCEPT
+sudo ip6tables -A OUTPUT -p icmpv6 --icmpv6-type echo-reply   -j ACCEPT
+sudo ip6tables -A OUTPUT -p icmpv6 --icmpv6-type echo-request -j ACCEPT
+sudo ip6tables -A INPUT  -p icmpv6 --icmpv6-type echo-reply   -j ACCEPT
+sudo iptables  -A INPUT  -p icmp   --icmp-type   echo-request -j ACCEPT
+sudo iptables  -A OUTPUT -p icmp   --icmp-type   echo-reply   -j ACCEPT
+sudo iptables  -A OUTPUT -p icmp   --icmp-type   echo-request -j ACCEPT
+sudo iptables  -A INPUT  -p icmp   --icmp-type   echo-reply   -j ACCEPT
 
 # Octocat.
 # https://help.github.com/articles/what-ip-addresses-does-github-use-that-i-should-whitelist/
-iptables -A INPUT  -p tcp -s 192.30.252.0/22 --match multiport --sports 22,80,443,9418 -j ACCEPT
-iptables -A OUTPUT -p tcp -d 192.30.252.0/22 --match multiport --dports 22,80,443,9418 -j ACCEPT
+sudo iptables -A INPUT  -p tcp -s 192.30.252.0/22 --match multiport --sports 22,80,443,9418 -j ACCEPT
+sudo iptables -A OUTPUT -p tcp -d 192.30.252.0/22 --match multiport --dports 22,80,443,9418 -j ACCEPT
 
 # DNS.
 #cat /etc/resolv.conf
 dns='43.240.99.250 8.8.8.8 43.240.99.251'
 for each in $dns; do
-    iptables -A INPUT  -p udp -s "$each" --sport 53 -j ACCEPT
-    iptables -A OUTPUT -p udp -d "$each" --dport 53 -j ACCEPT
-    iptables -A INPUT  -p tcp -s "$each" --sport 53 -j ACCEPT
-    iptables -A OUTPUT -p tcp -d "$each" --dport 53 -j ACCEPT
+    sudo iptables -A INPUT  -p udp -s "$each" --sport 53 -j ACCEPT
+    sudo iptables -A OUTPUT -p udp -d "$each" --dport 53 -j ACCEPT
+    sudo iptables -A INPUT  -p tcp -s "$each" --sport 53 -j ACCEPT
+    sudo iptables -A OUTPUT -p tcp -d "$each" --dport 53 -j ACCEPT
 done
 
 # Localhost.
-iptables -A INPUT  -i lo -j ACCEPT
-iptables -A OUTPUT -o lo -j ACCEPT
+sudo iptables -A INPUT  -i lo -j ACCEPT
+sudo iptables -A OUTPUT -o lo -j ACCEPT
 
 # Personal invitations.
 while read address; do
-    iptables -A INPUT  -p tcp -s $address/32 --match multiport --dports $ports -j ACCEPT
-    iptables -A OUTPUT -p tcp -d $address/32 --match multiport --sports $ports -j ACCEPT
+    sudo iptables -A INPUT  -p tcp -s $address/32 --match multiport --dports $ports -j ACCEPT
+    sudo iptables -A OUTPUT -p tcp -d $address/32 --match multiport --sports $ports -j ACCEPT
 done <$phonebook
 
 # Ruling sustained!
-ip6tables-save >/etc/iptables/rules.v6;
-iptables-save  >/etc/iptables/rules.v4;
+sudo ip6tables-save >/etc/iptables/rules.v6;
+sudo iptables-save  >/etc/iptables/rules.v4;
--- Server/install.sh
+++ Server/install.sh
@@ -1,60 +1,117 @@
 #!/usr/bin/env bash
-# Post-installation script for Ubuntu/NeCTAR.
-# (This should work, but I haven't tested it.)
+# Post-installation script for NeCTAR Ubuntu 16.04 LTS (Xenial) amd64.
+# # cat IP addresses into ~/phonebook for the firewall scripts
+# $ wget -O- 'https://github.com/Pretty-Cure-5/Euclidian/blob/master/Server/install.sh' | bash;
+
+################################################################################
+# Human required!                                                              #
+################################################################################
+printf \\ec;
+read -r -p 'Have you got a ~/phonebook? [y/n] ' response;
+response=${response,,}; # case insensitivity
+if [[ $response =~ ^(no|n)$ ]]; then # RTFM
+    printf '\nWell, what are you waiting for; an invitation?!?\n\n';
+    exit 1;
+fi;
+printf '\nI assume you know what you are doing...\n';
+sleep 2;
+printf '\nHere, before you go; take care of this...\n';
+sleep 2;
+printf '\n';
+yes | sudo apt-get -y install --force-yes iptables-persistent
+printf '\nYou are no longer required; this will take a long time...\n';
+sleep 5;
+printf '\n';
 
 ################################################################################
 # Configuration                                                                #
 ################################################################################
-home=/home/ubuntu/
+home=/home/ubuntu/;
+# https://bitbucket.org/sage2/sage2/wiki/
+sage2=https://bitbucket.org/sage2/sage2/downloads/SAGE2ubuntu.sh;
+# http://openjdk.java.net/install/
+jre=openjdk-8-jre;
+jdk=openjdk-8-jdk;
 # http://www.scala-lang.org/download/
-scala=scala-2.11.8.deb
+scala=scala-2.11.8.deb;
 
 ################################################################################
 # Update Server                                                                #
 ################################################################################
-sudo apt-get update
-sudo apt-get dist-upgrade
-sudo apt-get autoremove
-sudo apt-get autoclean
+sudo apt-get -y update;
+# OMG WTF FFS!!!
+yes | sudo apt-get -y dist-upgrade --force-yes;
 
 ################################################################################
-# GitHub
+# Install Packages                                                             #
 ################################################################################
-cd "$home"
-sudo apt-get install git
-git clone https://github.com/Pretty-Cure-5/Euclidian.git
+# SAGE2ubuntu.sh installs git
+yes | sudo apt-get -y install --force-yes\
+    `#iptables-persistent`\
+    "$jre"\
+    "$jdk"\
+    `#git`\
+    vim;
+# Fucking iptables-persistent!!!
+# I may have to move iptables-persistent to the very front of the line...
 
 ################################################################################
-# SAGE2 via Ian Peake (and maybe Ed?)
+# Scala                                                                        #
 ################################################################################
-cd "$home"
-wget https://bitbucket.org/sage2/sage2/downloads/SAGE2ubuntu.sh
-sh SAGE2ubuntu.sh
-rm SAGE2ubuntu.sh
-sudo apt-get install libav-dev
-cd sage2
-npm run in
-sudo apt-get install libswscale-dev
-npm install node-demux
-sudo apt-get install imagemagick
-chmod -R a+rX ~
+cd "$home";
+wget "http://scala-lang.org/files/archive/$scala";
+sudo dpkg -i "$scala";
+sudo apt-get -y update;
+sudo apt-get -y install scala;
 
 ################################################################################
-# Java [http://openjdk.java.net/install/]                                      #
+# SAGE2                                                                        #
 ################################################################################
-sudo apt-get install openjdk-8-jre openjdk-8-jdk
+cd "$home";
+wget -O- "$sage2" | sh;
+# `npm test` failed; requires node-demux
+# libswscale-dev required for node-demux
+# imagemagick    required for SAGE2
+sudo apt-get -y install libswscale-dev imagemagick;
+cd sage2;
+npm install node-demux;
 
 ################################################################################
-# Scala [http://www.scala-lang.org/download/]                                  #
+# GitHub                                                                       #
 ################################################################################
-cd "$home"
-wget "http://scala-lang.org/files/archive/$scala"
-sudo dpkg -i "$scala"
-sudo apt-get update
-sudo apt-get install scala
-rm "$scala"
+cd "$home";
+git clone https://github.com/Pretty-Cure-5/Euclidian.git;
 
 ################################################################################
-# Extras                                                                       #
+# Decruftification                                                             #
 ################################################################################
-sudo apt-get install vim iptables-persistent
+cd "$home";
+rm "$scala";
+sudo apt-get -y autoremove;
+sudo apt-get -y autoclean;
+
+################################################################################
+# Firewall                                                                     #
+################################################################################
+wget -O- 'https://github.com/Pretty-Cure-5/Euclidian/blob/master/Server/firewall.sh' | bash;
+
+################################################################################
+# Testification                                                                #
+################################################################################
+# Java 2>&1 | Head
+printf '\ecShyam Has Your Anomaly Mitigated! :D\n\nINSTALLED:\n';
+vim   --version | head -n 1;
+git   --version;
+printf 'NodeJS ';
+node  --version;
+printf 'NPM ';
+npm   --version;
+scala  -version;
+java   -version  |& head -n 1;
+javac  -version;
+
+################################################################################
+# You know nothing Jon Snow...until; it is known! :D                           #
+################################################################################
+printf '\nReboot, then check your firewall!\n\n';
+
