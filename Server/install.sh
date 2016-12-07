bff4025d0f1901b007c19f5ddc1eaa70f37b0be6
Shyam Nath <Sharminator@users.noreply.github.com>
2016-12-07 13:26:51 +1100

    Update install.sh

--- Server/install.sh
+++ Server/install.sh
@@ -53,7 +53,7 @@ printf '\n';
 ################################################################################
 sudo apt-get -y update;
 # OMG WTF FFS!!!
-#sudo apt-get -y dist-upgrade;
+sudo apt-get dist-upgrade;
 
 ################################################################################
 # Install Packages                                                             #
